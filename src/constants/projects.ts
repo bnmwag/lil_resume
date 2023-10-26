785fa301c49c44beb4582e3122048ac680c6cd15
Benjamin Wagner <bnm.wag@gmail.com>
2023-10-26 03:15:16 +0200

    lil cleanup 🧹+ structure rearrange

---
+++ src/constants/projects.ts
@@ -0,0 +1,28 @@
+import type { Project } from '../types.ts';
+
+export const projects: Project[] = [
+	{
+		title: 'studio',
+		url: 'https://studio.bnmwag.dev',
+		branch: 'studio',
+		tags: ['anything imaginable'],
+	},
+	{
+		title: 'HW-Technik',
+		url: 'https://hwtechnik.at',
+		branch: 'freelance',
+		tags: ['web', 'design', 'hosting'],
+	},
+	{
+		title: 'assaabloy',
+		url: 'https://assaabloy.at',
+		branch: 'sps',
+		tags: ['web', 'webgl', 'design'],
+	},
+	{
+		title: 'Neuronet.ink',
+		url: 'https://neuronet.ink',
+		branch: 'freelance',
+		tags: ['design', 'web', 'blog', 'cms', 'hosting'],
+	},
+];

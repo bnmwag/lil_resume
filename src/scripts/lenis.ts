2adfb4ee318a0f3c161d98e385e6407cb120f627
Benjamin Wagner <bnm.wag@gmail.com>
2023-10-27 09:16:36 +0200

    lenis config

--- src/scripts/lenis.ts
+++ src/scripts/lenis.ts
@@ -1,6 +1,9 @@
 import Lenis from '@studio-freight/lenis';
 
-const lenis = new Lenis() as Lenis;
+const lenis = new Lenis({
+	smoothTouch: true,
+	smoothWheel: true,
+}) as Lenis;
 
 function raf(time: number) {
 	lenis.raf(time);

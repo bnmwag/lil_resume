a5ead52cb4c66c7bc4e81fe8cdf3d8dbb3c9cf34
Benjamin Wagner <bnm.wag@gmail.com>
2023-10-27 19:04:44 +0200

    change theme toggle from dropdown to buttons

--- src/components/theme-toggles.tsx
+++ src/components/theme-toggles.tsx
@@ -1,25 +1,30 @@
-import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { Moon, Sun } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+
+type Theme = 'theme-light' | 'dark' | 'system';
+
+const toggles = [
+	{
+		value: 'theme-light',
+		icon: <Sun size={14} />,
+	},
+	{
+		value: 'dark',
+		icon: <Moon size={14} />,
+	},
+] satisfies { value: Theme; icon: JSX.Element }[];
 
 const ThemeToggles: React.FC = () => {
-	const [theme, setThemeState] = React.useState<
-		'theme-light' | 'dark' | 'system'
-	>('theme-light');
+	const [theme, setThemeState] = useState<Theme>('theme-light');
 
-	React.useEffect(() => {
+	useEffect(() => {
 		const isDarkMode = document.documentElement.classList.contains('dark');
 		setThemeState(isDarkMode ? 'dark' : 'theme-light');
 	}, []);
 
-	React.useEffect(() => {
+	useEffect(() => {
 		const isDark =
 			theme === 'dark' ||
 			(theme === 'system' &&
@@ -28,26 +33,18 @@ const ThemeToggles: React.FC = () => {
 	}, [theme]);
 
 	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button variant='outline' size='icon'>
-					<Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-					<Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-					<span className='sr-only'>Toggle theme</span>
+		<div className='flex items-center gap-x-2'>
+			{toggles.map((toggle) => (
+				<Button
+					variant={toggle.value === theme ? 'outline' : 'ghost'}
+					key={toggle.value}
+					size={'sm'}
+					onClick={() => setThemeState(toggle.value)}
+				>
+					{toggle.icon}
 				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent align='end'>
-				<DropdownMenuItem onClick={() => setThemeState('theme-light')}>
-					Light
-				</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setThemeState('dark')}>
-					Dark
-				</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setThemeState('system')}>
-					System
-				</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
+			))}
+		</div>
 	);
 };
 

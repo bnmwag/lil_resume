0c85256c63501b24e5d42450f059fe5e59194659
Benjamin Wagner <bnm.wag@gmail.com>
2023-10-27 19:26:38 +0200

    adding party town for perf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		react(),
-	],
-	output: 'server',
-	adapter: vercel(),
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react(), partytown()],
+  output: 'server',
+  adapter: vercel()
 });

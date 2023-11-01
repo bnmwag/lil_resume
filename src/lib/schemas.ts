ad75165aa235dcff30a63e9582d986c0220170d3
bnmwag <bnm.wag@gmail.com>
2023-11-02 00:49:07 +0100

    other formatter

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,43 +1,43 @@
-import * as z from 'zod';
+import * as z from "zod";
 
 export const all_interests = [
-	'Web Development',
-	'Blog',
-	'UI/UX Design',
-	'CMS Integration',
-	'Hosting',
-	'eCommerce',
-	'WebGl / 3D',
+  "Web Development",
+  "Blog",
+  "UI/UX Design",
+  "CMS Integration",
+  "Hosting",
+  "eCommerce",
+  "WebGl / 3D",
 ];
 
-export const all_budgets = ['<500', '500-1k', '3k-5k', '5k-10k', '10k+'];
+export const all_budgets = ["<500", "500-1k", "3k-5k", "5k-10k", "10k+"];
 
 export const formSchema = z.object({
-	name: z
-		.string()
-		.min(3, {
-			message: 'Please enter a valid name',
-		})
-		.max(50, {
-			message: 'Name must be less than 50 characters',
-		}),
-	email: z.string().email({
-		message: 'Please enter a valid email address',
-	}),
-	interests: z
-		.array(
-			z.string().refine((v) => all_interests.includes(v), {
-				message: 'Please select a valid interest',
-			})
-		)
-		.nonempty({
-			message: 'Please select at least one interest',
-		}),
-	budget: z
-		.string()
-		.min(1, {
-			message: 'Please select a budget',
-		})
-		.refine((v) => all_budgets.includes(v)),
-	info: z.string().optional(),
+  name: z
+    .string()
+    .min(3, {
+      message: "Please enter a valid name",
+    })
+    .max(50, {
+      message: "Name must be less than 50 characters",
+    }),
+  email: z.string().email({
+    message: "Please enter a valid email address",
+  }),
+  interests: z
+    .array(
+      z.string().refine((v) => all_interests.includes(v), {
+        message: "Please select a valid interest",
+      })
+    )
+    .nonempty({
+      message: "Please select at least one interest",
+    }),
+  budget: z
+    .string()
+    .min(1, {
+      message: "Please select a budget",
+    })
+    .refine((v) => all_budgets.includes(v)),
+  info: z.string().optional(),
 });
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,21 +1,21 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
 
 export const getCurrentTimeInVienna = (): string => {
-	const viennaTime = new Date().toLocaleTimeString('en-US', {
-		timeZone: 'Europe/Vienna',
-		hour: '2-digit',
-		minute: '2-digit',
-		second: '2-digit',
-		hour12: false,
-	});
+  const viennaTime = new Date().toLocaleTimeString("en-US", {
+    timeZone: "Europe/Vienna",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  });
 
-	const [hours, minutes, seconds] = viennaTime.split(':');
-	const formattedHours = hours === '24' ? '00' : hours;
+  const [hours, minutes, seconds] = viennaTime.split(":");
+  const formattedHours = hours === "24" ? "00" : hours;
 
-	return `${formattedHours}:${minutes}:${seconds}`;
+  return `${formattedHours}:${minutes}:${seconds}`;
 };

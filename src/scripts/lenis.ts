a28b91c4c459b95c4dd7962d0f9569b0729d0f5d
bnmwag <bnm.wag@gmail.com>
2023-12-14 00:14:51 +0100

    add docs

--- src/scripts/lenis.ts
+++ src/scripts/lenis.ts
@@ -1,14 +1,28 @@
 import Lenis from "@studio-freight/lenis";
 
-const lenis = new Lenis({
-  smoothTouch: false,
-  smoothWheel: true,
-  normalizeWheel: true,
-}) as Lenis;
+/**
+ * Immediately invoked function expression (IIFE) to initialize the Lenis library.
+ *
+ * This function creates a new instance of the Lenis class and passes in an object
+ * with the base configuration for the library. The instance is then passed to a
+ * requestAnimationFrame loop to update the library every frame.
+ */
+(function () {
+  /**
+   * Creates a new instance of the Lenis class.
+   * @type {Lenis}
+   */
+  const lenis = new Lenis({ smoothTouch: false, smoothWheel: true, normalizeWheel: true }) as Lenis;
 
-function raf(time: number) {
-  lenis.raf(time);
+  /**
+   * RequestAnimationFrame loop to update the Lenis instance every frame.
+   * @param {number} time - The current time in milliseconds.
+   */
+  function raf(time: number) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  // Start the requestAnimationFrame loop.
   requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
+})();
--- src/scripts/title-status.ts.ts
+++ src/scripts/title-status.ts.ts
@@ -1,15 +1,39 @@
-// detect when user leaves the page and update the title of the page to "Come back!"
+/**
+ * Immediately Invoked Function Expression (IIFE) to change the document title based on tab visibility.
+ *
+ * This function listens for the 'visibilitychange' event on the document. When the tab becomes
+ * hidden (e.g., when a user switches to a different tab), it starts a timer. If the tab remains
+ * hidden for more than 3 seconds, the document title changes to a predefined message.
+ * When the tab becomes visible again, the timer is cleared and the document title is restored
+ * to its original value.
+ */
+(function () {
+  /**
+   * Stores the original title of the document.
+   * @type {string}
+   */
+  const originalTitle: string = document.title;
 
-const originalTitle = document.title;
-let timeout: NodeJS.Timeout;
+  /**
+   * Variable to hold the timeout ID for the timer.
+   * @type {NodeJS.Timeout | undefined}
+   */
+  let timeout: NodeJS.Timeout | undefined;
 
-document.addEventListener("visibilitychange", () => {
-  if (document.visibilityState === "hidden") {
-    timeout = setTimeout(() => {
-      document.title = "Already left?";
-    }, 3000);
-  } else {
-    if (timeout) clearTimeout(timeout);
-    document.title = originalTitle; // Replace with your original title
-  }
-});
+  /**
+   * Event listener for the 'visibilitychange' event on the document.
+   * Changes the document title based on the visibility state of the document.
+   */
+  document.addEventListener("visibilitychange", (): void => {
+    if (document.visibilityState === "hidden") {
+      // Set a timeout to change the title after 3 seconds of the document being hidden.
+      timeout = setTimeout((): void => {
+        document.title = "Already left?";
+      }, 3000);
+    } else {
+      // Clear the timeout and restore the original title if the document becomes visible again.
+      clearTimeout(timeout);
+      document.title = originalTitle;
+    }
+  });
+})();

62ace7023c4a87c6e5143453e60ea75f3f4ea3e0
bnmwag <bnm.wag@gmail.com>
2023-12-13 23:39:33 +0100

    change formating

--- src/components/theme-toggles.tsx
+++ src/components/theme-toggles.tsx
@@ -1,53 +1,50 @@
-import { useState, useEffect } from 'react';
-import { Moon, Sun } from 'lucide-react';
+import { useState, useEffect } from "react";
+import { Moon, Sun } from "lucide-react";
 
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 
-type Theme = 'theme-light' | 'dark' | 'system';
+type Theme = "theme-light" | "dark" | "system";
 
 const toggles = [
-	{
-		value: 'theme-light',
-		icon: <Sun size={14} />,
-	},
-	{
-		value: 'dark',
-		icon: <Moon size={14} />,
-	},
+  {
+    value: "theme-light",
+    icon: <Sun size={14} />,
+  },
+  {
+    value: "dark",
+    icon: <Moon size={14} />,
+  },
 ] satisfies { value: Theme; icon: JSX.Element }[];
 
 const ThemeToggles: React.FC = () => {
-	const [theme, setThemeState] = useState<Theme>('dark');
+  const [theme, setThemeState] = useState<Theme>("dark");
 
-	useEffect(() => {
-		const isDarkMode = document.documentElement.classList.contains('dark');
-		setThemeState(isDarkMode ? 'dark' : 'theme-light');
-	}, []);
+  useEffect(() => {
+    const isDarkMode = document.documentElement.classList.contains("dark");
+    setThemeState(isDarkMode ? "dark" : "theme-light");
+  }, []);
 
-	useEffect(() => {
-		const isDark =
-			theme === 'dark' ||
-			(theme === 'system' &&
-				window.matchMedia('(prefers-color-scheme: dark)').matches);
-		document.documentElement.classList[isDark ? 'add' : 'remove']('dark');
-	}, [theme]);
+  useEffect(() => {
+    const isDark =
+      theme === "dark" ||
+      (theme === "system" && window.matchMedia("(prefers-color-scheme: dark)").matches);
+    document.documentElement.classList[isDark ? "add" : "remove"]("dark");
+  }, [theme]);
 
-	return null;
-
-	// return (
-	// 	<div className='flex items-center gap-x-2'>
-	// 		{toggles.map((toggle) => (
-	// 			<Button
-	// 				variant={toggle.value === theme ? 'outline' : 'ghost'}
-	// 				key={toggle.value}
-	// 				size={'sm'}
-	// 				onClick={() => setThemeState(toggle.value)}
-	// 			>
-	// 				{toggle.icon}
-	// 			</Button>
-	// 		))}
-	// 	</div>
-	// );
+  return (
+    <div className='flex items-center gap-x-2'>
+      {toggles.map((toggle) => (
+        <Button
+          variant={toggle.value === theme ? "outline" : "ghost"}
+          key={toggle.value}
+          size={"sm"}
+          onClick={() => setThemeState(toggle.value)}
+        >
+          {toggle.icon}
+        </Button>
+      ))}
+    </div>
+  );
 };
 
 export default ThemeToggles;
--- src/scripts/lenis.ts
+++ src/scripts/lenis.ts
@@ -1,14 +1,14 @@
-import Lenis from '@studio-freight/lenis';
+import Lenis from "@studio-freight/lenis";
 
 const lenis = new Lenis({
-	smoothTouch: false,
-	smoothWheel: true,
-	normalizeWheel: true,
+  smoothTouch: false,
+  smoothWheel: true,
+  normalizeWheel: true,
 }) as Lenis;
 
 function raf(time: number) {
-	lenis.raf(time);
-	requestAnimationFrame(raf);
+  lenis.raf(time);
+  requestAnimationFrame(raf);
 }
 
 requestAnimationFrame(raf);
